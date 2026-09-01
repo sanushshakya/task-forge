4ceb7c2d5dccc9d7195510014d631f9c0ad619ab
sanushshakya <shakyasanush7@gmail.com>
2026-09-01 19:48:43 +0100

    feat: add fetch event logic for cache-first strategy in sw.js

--- public/sw.js
+++ public/sw.js
@@ -24,7 +24,10 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
