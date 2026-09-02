6398fb24e7dece86e4da7c79ef122338d7fb399a
sanushshakya <shakyasanush7@gmail.com>
2026-09-02 22:51:14 +0100

    docs: add type definitions for ServiceWorkerRegister

---
+++ components/ServiceWorkerRegister.d.ts
@@ -0,0 +1,8 @@
+// components/ServiceWorkerRegister.d.ts
+
+/**
+ * A React component that registers a Service Worker for offline functionality.
+ */
+declare const ServiceWorkerRegister: React.FC;
+
+export default ServiceWorkerRegister;
