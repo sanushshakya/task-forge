a33083433749a2c487dbf32e797ce264efc409e2
sanushshakya <shakyasanush7@gmail.com>
2026-09-07 22:00:15 +0100

    chore(models): add isAdmin field to User model

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,41 @@
 // models/User.ts
 
 /**
+ * Represents a user in the application with an optional team association and admin status.
+ */
+export interface User {
+  /**
+   * The unique identifier for the user.
+   */
+  _id: string;
+
+  /**
+   * The username of the user.
+   */
+  username: string;
+
+  /**
+   * The email address of the user.
+   */
+  email: string;
+
+  /**
+   * Indicates whether the user's account is active.
+   */
+  isActive: boolean;
+
+  /**
+   * Optional reference to a team associated with the user.
+   */
+  teamId?: mongoose.Schema.Types.ObjectId;
+
+  /**
+   * Indicates whether the user has administrative privileges.
+   */
+  isAdmin: boolean;
+}
+
+/**
  * Define a Mongoose schema for the User model.
  * @property email - The user's email address, required and unique.
  * @property hashed_password - The hashed version of the user's password, required.
@@ -8,6 +43,7 @@
  * @property reset_token_expiry - The expiry time for the password reset token, optional.
  * @property createdAt - The timestamp when the user was created, defaulting to the current date.
  * @property teamId - An optional reference to a Team model, allowing users to be associated with a team.
+ * @property isAdmin - Indicates whether the user has administrative privileges, defaulting to false.
  */
 const userSchema = new mongoose.Schema({
   email: {
@@ -33,6 +69,10 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Team',
     required: false, // Make the field optional
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false, // Add isAdmin field with a default value of false
   },
 });
 
