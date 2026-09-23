1bdd91a7ede11873f00d612f9fc8a9b3039835f3
sanushshakya <shakyasanush7@gmail.com>
2026-09-23 23:22:50 +0100

    feat(components): color day cells by mood in CalendarHeatmap.tsx

--- components/CalendarHeatmap.tsx
+++ components/CalendarHeatmap.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { scaleLinear } from '@visx/scale';
 import { Group } from '@visx/group';
-import { LineMark } from '@visx/shape';
+import { RectMark } from '@visx/shape';
 
 /**
  * Component for rendering a calendar heatmap.
@@ -40,7 +40,7 @@ const CalendarHeatmap: React.FC<{ data: { date: string; value: number }[] }> = (
   // Create a linear scale based on the value range
   const colorScale = scaleLinear({
     domain: [minValue, maxValue],
-    range: ['#f5deb3', '#e9967a'],
+    range: ['#e0ffff', '#006edd'], // Updated to use Tailwind color scale for moods (1-5)
   });
 
   return (
@@ -69,7 +69,7 @@ const CalendarHeatmap: React.FC<{ data: { date: string; value: number }[] }> = (
           const y = 400 - (entry.value / (maxValue - minValue)) * 200;
 
           return (
-            <rect
+            <RectMark
               key={index}
               x={x - 10}
               y={y}
